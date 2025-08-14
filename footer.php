666c4de43eae13a6120dfce7bec82906e7fa93a5
Ioannats2262 <161051990+Ioannats2262@users.noreply.github.com>
2025-08-14 11:31:02 +0300

    Update footer.php

--- footer.php
+++ footer.php
@@ -4,17 +4,3 @@
     <p>&copy; <?php echo date('Y'); ?> Летище Варна | Всички права запазени.</p>
   </div>
 </footer>
-
-<style>
-  footer {
-    background-color: #004080;
-    color: white;
-    text-align: center;
-    padding: 20px 0;
-    margin-top: 40px;
-  }
-  .footer-content p {
-    margin: 5px 0;
-    font-size: 14px;
-  }
-</style>
